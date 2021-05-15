4a23b6e6d99546695bc70a172024b12e14f8397d
楊斯惟 <yangszwei@outlook.com>
2021-05-15 13:43:36 +0800

    Create selection_sort.cpp

---
+++ algorithms/selection_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *x, int *y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+void selection_sort(int array[], int len) {
+    for (int i = 0;i < len - 1;i++) {
+        int min = i; // index of minimum
+        for (int j = i + 1;j < len;j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+        }
+        swap(&array[i], &array[min]);
+    }
+}
+
+int main() {
+    int test_array[] = {3, 6, 23, -1, 54};
+
+    selection_sort(test_array, sizeof(test_array));
+
+    for (int i : test_array)
+        cout << i << " ";
+
+    return 0;
+}
